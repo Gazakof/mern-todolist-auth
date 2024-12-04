4ffffab8d70067178573a546519a9b77a731c8c4
Gazakof <randriamorasata08@gmail.com>
2024-12-04 20:10:29 +0100

    psw

--- src/components/input/PasswordInput.jsx
+++ src/components/input/PasswordInput.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const PasswordInput = ({ value, onChange, placeholder }) => {
   return (
     <div className='flex items-center bg-transparent border-[1.5px] px-5 rounded mb-3'>
-fdfgdsdf
+
     </div>
   )
 }
