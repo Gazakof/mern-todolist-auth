3bf74cbd8e5b3895bd394c89315c20c5e60b66b9
Gazakof <randriamorasata08@gmail.com>
2024-12-04 19:35:47 +0100

    navbar

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -5,6 +5,16 @@ const Login = () => {
   return (
     <>
         <Navbar />
+        <div>
+          <di>
+            <form onSubmit={() => {}}>
+              <h4 className='text-2xl mb-7'>
+                Login
+              </h4>
+              <input type='text' placeholder='Email' className='input-box' />
+            </form>
+          </di>
+        </div>
     </>
   )
 }
