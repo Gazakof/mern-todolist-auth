0263cf402ec8bbb02b7fb82fd7a07c3da77a4204
Gazakof <randriamorasata08@gmail.com>
2024-12-06 12:52:37 +0100

    profile

--- src/components/Cards/ProfileInfo.jsx
+++ src/components/Cards/ProfileInfo.jsx
@@ -5,13 +5,13 @@ const ProfileInfo = ({ onLogout }) => {
     <>
         <div className='flex items-center gap-3'>
             <div className='w-12 h-12 flex items-center justify-center rounded-full text-slate-950 bg-slate-100'>
-            
+                WI
             </div>
             <div>
                 <p className='text-sm font-medium'>
                     William
                 </p>
-                <button className='' onClick={onLogout}>
+                <button className='text-sm text-slate-700 underline' onClick={onLogout}>
                     Logout
                 </button>
             </div>
