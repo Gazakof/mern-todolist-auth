7758c3077e06857076ffc45b44f6a09d332b9d48
Gazakof <randriamorasata08@gmail.com>
2024-12-10 09:27:35 +0100

    note card

--- src/components/Navigationbar/Navbar.jsx
+++ src/components/Navigationbar/Navbar.jsx
@@ -11,13 +11,28 @@ const Navbar = () => {
     navigate("/login");
   }
 
+  const handleSearch = () => {
+
+  }
+
+  const onClearSearch = () => {
+    setSearchQuery("");
+  }
+
   return (
     <>
       <div className='bg-white flex items-center justify-between px-6 py-2 drop-shadow'>
         <h2 className='text-xl font-medium text-black py-2'>
           Notes
         </h2>
-        <SearchBar/>
+        <SearchBar 
+          value={searchQuery}
+          onChage={({ target }) => {
+            setSearchQuery(target.value);
+          }}
+          handleSearch={handleSearch}
+          onClearSearch={onClearSearch}
+        />
         <ProfileInfo onLogout={onLogout}/>
       </div>
     </>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import Navbar from '../../components/Navigationbar/Navbar'
+import NoteCard from '../../components/Cards/NoteCard'
 
 const Home = () => {
   return (
     <>
       <Navbar />
+      <div className='container mx-auto'>
+        <NoteCard 
+          title="Meeting at 7am"
+          date="3rd Apr"
+          content="Meeting at 7am on 3rd April"
+          tags={true}
+          onEdit={() => {}}
+          onDelete={() => {}}
+          onPinNote={() => {}}
+        />
+      </div>
     </>
   )
 }
