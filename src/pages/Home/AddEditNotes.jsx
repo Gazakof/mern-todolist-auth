0dd0db12db27172d2c73ca739bfe8ff5ec8c2330
Gazakof <randriamorasata08@gmail.com>
2024-12-10 11:02:15 +0100

    editNotes

--- src/pages/Home/AddEditNotes.jsx
+++ src/pages/Home/AddEditNotes.jsx
@@ -14,6 +14,17 @@ const AddEditNotes = () => {
                         placeholder="Go to Gym at 5am"
                     />
                 </div>
+                <div className="flex flex-col gap-2 mt-4">
+                    <label className="input-label">
+                        Content
+                    </label>
+                    <textarea 
+                        type="text"
+                        className="text-sm text-slate-950 outline-none bg-slate-50 p-2 rounded"
+                        placeholder="Content"
+                        rows={10}
+                    />
+                </div>
             </div>
         </>
     )
