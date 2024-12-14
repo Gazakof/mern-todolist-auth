7b1cb68dbe4235c1a6ee8fc049667115151a2c53
Gazakof <randriamorasata08@gmail.com>
2024-12-14 18:30:57 +0100

    tags

--- src/components/input/TagInput.jsx
+++ src/components/input/TagInput.jsx
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdAdd } from "react-icons/md";
 
 const TagInput = ({ tags, setTags }) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const addNewTag = () => {
+    if (inputValue.trim() !== "") {
+      setTags([...tags, inputValue.trim()]);
+      setInputValue("");
+    }
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Entrer") {
+      addNewTag();
+    }
+  };
   return (
     <>
       <div>
         <div className="flex items-center gap-4 mt-3">
-          <input type="text" className="" placeholder="Add tags" />
-          <button>
-            <MdAdd className="text-2xl rounded text-blue-700 hover:text-white hover:bg-blue-700" />
+          <input
+            type="text"
+            className="text-sm bg-transparent border px-3 py-2 rounded outline-none"
+            placeholder="Add tags"
+            onChange={handleInputChange}
+          />
+          <button className="w-8 h-8 flex items-center justify-center border rounded border-blue-700 hover:bg-blue-700">
+            <MdAdd className="text-2xl rounded text-blue-700 hover:text-white" />
           </button>
         </div>
       </div>
