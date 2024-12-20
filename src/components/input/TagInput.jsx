ba7219a7c782d6744c7a01af712fcf74684c1d48
Gazakof <randriamorasata08@gmail.com>
2024-12-20 05:51:29 +0100

    add note

--- src/components/input/TagInput.jsx
+++ src/components/input/TagInput.jsx
@@ -16,7 +16,7 @@ const TagInput = ({ tags, setTags }) => {
   };
 
   const handleKeyDown = (e) => {
-    if (e.key === "Entrer") {
+    if (e.key === "Enter") {
       addNewTag();
     }
   };
@@ -31,7 +31,10 @@ const TagInput = ({ tags, setTags }) => {
         {tags?.length > 0 && (
           <div className="flex items-center gap-2 flex-wrap mt-2">
             {tags.map((tag, index) => (
-              <span key={index} className="">
+              <span
+                key={index}
+                className="flex items-center gap-2 text-sm text-slate-900 bg-slate-300 px-3 py-1 rounded"
+              >
                 # {tag}
                 <button
                   onClick={() => {
@@ -47,6 +50,7 @@ const TagInput = ({ tags, setTags }) => {
         <div className="flex items-center gap-4 mt-3">
           <input
             type="text"
+            value={inputValue}
             className="text-sm bg-transparent border px-3 py-2 rounded outline-none"
             placeholder="Add tags"
             onChange={handleInputChange}
--- src/pages/Home/AddEditNotes.jsx
+++ src/pages/Home/AddEditNotes.jsx
@@ -1,20 +1,52 @@
 import React, { useState } from "react";
 import TagInput from "../../components/Input/TagInput";
+import { MdClose } from "react-icons/md";
 
-const AddEditNotes = () => {
+const AddEditNotes = ({ noteData, type, onClose }) => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [tags, setTags] = useState([]);
+  const [error, setError] = useState(null);
+
+  const addNewNote = async () => {};
+  const editNote = async () => {};
+
+  const handleAddNote = () => {
+    if (!title) {
+      setError("Please enter a title.");
+      return;
+    }
+    if (!content) {
+      setError("Please enter the content.");
+      return;
+    }
+
+    setError("");
+
+    if (type === "edit") {
+      editNote();
+    } else {
+      addNewNote();
+    }
+  };
 
   return (
     <>
-      <div>
+      <div className="relative">
+        <div
+          className="w-8 h-8 rounded-full flex items-center justify-center absolute -top-3 -right-3 hover:bg-slate-100"
+          onClick={onClose}
+        >
+          <MdClose className="text-xl text-slate-400" />
+        </div>
         <div className="flex flex-col gap-2">
           <label className="input-label">Title</label>
           <input
             type="text"
             className="text-2xl text-slate-950 outline-none"
-            placeholder="Go to Gym at 5am"
+            placeholder="Title"
+            value={title}
+            onChange={({ target }) => setTitle(target.value)}
           />
         </div>
         <div className="flex flex-col gap-2 mt-4">
@@ -24,13 +56,23 @@ const AddEditNotes = () => {
             className="text-sm text-slate-950 outline-none bg-slate-50 p-2 rounded"
             placeholder="Content"
             rows={10}
+            value={content}
+            onChange={({ target }) => setContent(target.value)}
           />
         </div>
         <div className="mt-3">
           <label className="input-label">Tags</label>
-          <TagInput />
+          <TagInput tags={tags} setTags={setTags} />
         </div>
-        <button className="btn-primary font-medium mt-5 p-3">ADD</button>
+
+        {error && <p className="text-red-500 text-xs pt-4">{error}</p>}
+
+        <button
+          className="btn-primary font-medium mt-5 p-3"
+          onClick={handleAddNote}
+        >
+          ADD
+        </button>
       </div>
     </>
   );
